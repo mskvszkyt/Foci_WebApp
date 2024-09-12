f3585a8820edec24a05b831ad03a3a149ace47a2
mskvszkyt <muskovszkyt@gmail.com>
2024-09-12 09:32:56 +0200

    create and list page

---
+++ Pages/MeccsHozzaadas.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Foci_WebApp.Models;
+
+namespace Foci_WebApp.Pages
+{
+    public class MeccsHozzaadasModel : PageModel
+    {
+        private readonly Foci_WebApp.Models.FociDbContext _context;
+
+        public MeccsHozzaadasModel(Foci_WebApp.Models.FociDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Meccs Meccs { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Meccsek.Add(Meccs);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/MeccsekListaja.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Foci_WebApp.Models;
+
+namespace Foci_WebApp.Pages
+{
+    public class MeccsekListajaModel : PageModel
+    {
+        private readonly Foci_WebApp.Models.FociDbContext _context;
+
+        public MeccsekListajaModel(Foci_WebApp.Models.FociDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Meccs> Meccs { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Meccs = await _context.Meccsek.ToListAsync();
+        }
+    }
+}
