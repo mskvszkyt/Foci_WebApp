20164336a09cd5f54bba11e2a7b1ae6a61c5f742
mskvszkyt <muskovszkyt@gmail.com>
2024-09-12 08:53:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Foci_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Foci_WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<FociDbContext>(options => options.UseSqlite(connectionString: "Data Source = Foci.db"));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
